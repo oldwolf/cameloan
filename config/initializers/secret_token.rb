65b23e20b9e84602b40e6f31826158f60550d8d3
oldwolf <bypang1226@gmail.com>
2013-10-19 00:04:24 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cameloan::Application.config.secret_key_base = '76366dcd40a391fad4a8970f76a349b333aab4a595e06dd44f5045fd7548f2235580c395539be79570cf1112dd5be560a040466afb63f1f8feb25dd4250e01b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cameloan::Application.config.session_store :cookie_store, key: '_cameloan_session'
